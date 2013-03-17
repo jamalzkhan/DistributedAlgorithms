8ce1296888135a5b28b32878e2895fd4e68a2b91
Jamal Khan <jzk09@doc.ic.ac.uk>
2013-03-17 19:32:35 +0000

    Moar changes to EPFD

--- src/EventuallyPerfectFailureDetector.java
+++ src/EventuallyPerfectFailureDetector.java
@@ -3,32 +3,29 @@ import java.util.HashSet;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
-
 public class EventuallyPerfectFailureDetector implements IFailureDetector {
-
 
 	Process process;
 	HashSet<Integer> suspects;
 	Timer timer;
 	long[] processLastMessage;
-	HashMap<Integer, Long> maximumDelays;
+	long[] maximumDelays;
 	
 	static final int Delta = 1000; /* 1sec */
 	
 	class PeriodicTask extends TimerTask {
 		public void run() {
-			process.broadcast("heartbeat", "null");
+			process.broadcast("heartbeat", String.format("%d", System.currentTimeMillis()));
 			
 			long currentTime = System.currentTimeMillis();
+			
 			for (int i = 0; i < processLastMessage.length; i++) {
 				long timeDifference = currentTime - processLastMessage[i];
-				if ((processLastMessage[i] != 0) && (timeDifference > (Delta + Utils.DELAY))){
+				if ((processLastMessage[i] != 0) && (timeDifference > (Delta + maximumDelays[i]))){
 					if (!suspects.contains(i))
 						suspects.add(i);
 				}
 			}
-
 
 		}
 	}
@@ -37,7 +34,7 @@ public class EventuallyPerfectFailureDetector implements IFailureDetector {
 		this.process = p;
 		this.timer = new Timer();
 		this.suspects = new HashSet<Integer>();
-		this.maximumDelays = new HashMap<Integer, Long>();
+		this.maximumDelays = new long[p.n];
 		processLastMessage = new long[p.n];
 	}
 
@@ -46,15 +43,23 @@ public class EventuallyPerfectFailureDetector implements IFailureDetector {
 	public void begin() {
 		timer.schedule(new PeriodicTask(), 0, Delta);
 	}
+	
 
 	/* Handles in-coming (heartbeat) messages */
 	@Override
 	public void receive(Message m) {
 		long recieveTime = System.currentTimeMillis();
 		long delay = (System.currentTimeMillis() - Long.parseLong(m.getPayload()));
-		processLastMessage[m.getSource() - 1] = recieveTime;
+		int sourceProcess = m.getSource() - 1;
 		
-		//print suspects as well
+		processLastMessage[sourceProcess] = recieveTime;
+		
+		if (maximumDelays[sourceProcess] < delay)
+			maximumDelays[sourceProcess] = delay;
+		
+		if (suspects.contains(sourceProcess))
+			suspects.remove(sourceProcess);
+		
 		Utils.out(process.pid, m.toString());
 	}
 

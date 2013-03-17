c24a6c84a14e1cab19c09a3eadff7956be32c173
Danish Khan <danish.khan09@imperial.ac.uk>
2013-03-17 19:33:33 +0000

    EPFD process

--- src/EPFDProcess.java
+++ src/EPFDProcess.java
@@ -1,9 +1,30 @@
+class EPFDProcess extends Process {
 
-public class EPFDProcess extends Process {
+	private IFailureDetector detector;
+	private String heartBeat = "heartbeat";
 
 	public EPFDProcess(String name, int pid, int n) {
 		super(name, pid, n);
-		// TODO Auto-generated constructor stub
+		this.detector = new EventuallyPerfectFailureDetector(this);
 	}
 
+	public void begin() {
+		this.detector.begin();
+	}
+	
+	public synchronized void receive (Message m) {
+		String type = m.getType();
+		if (type.equals(heartBeat)) {
+			detector.receive(m);
+		}
+	}
+	
+	public static void main(String[] args) {
+		String name = args[0];
+		int id = Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[2]);
+		EPFDProcess p = new EPFDProcess(name, id, n);
+		p.registeR();
+		p.begin();
+	}
 }

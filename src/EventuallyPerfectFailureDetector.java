6874a593249a879958ac064e0b769bb6ecd99cf0
Jamal Khan <me@jamalkhan.com>
2013-03-21 12:29:48 +0000

    Final code

--- src/EventuallyPerfectFailureDetector.java
+++ src/EventuallyPerfectFailureDetector.java
@@ -12,15 +12,15 @@ public class EventuallyPerfectFailureDetector implements IFailureDetector {
 	long[] maximumDelays;
 	TimerTask periodicTask;
 	protected Message latestMessage;
-	
+
 	static final int Delta = 1000; /* 1sec */
-	
+
 	class PeriodicTask extends TimerTask {
 		public void run() {
 			process.broadcast("heartbeat", String.format("%d", System.currentTimeMillis()));
-			
+
 			long currentTime = System.currentTimeMillis();
-			
+
 			for (int i = 0; i < processLastMessage.length; i++) {
 				long timeDifference = currentTime - processLastMessage[i];
 				if ((processLastMessage[i] != 0) && (timeDifference > (Delta + maximumDelays[i]))){
@@ -46,30 +46,32 @@ public class EventuallyPerfectFailureDetector implements IFailureDetector {
 	/* Initiates communication tasks, e.g. sending heartbeats periodically */
 	public void begin() {
 		timer.schedule(this.periodicTask, 0, Delta);
-		
+
 	}
-	
+
 
 	/* Handles in-coming (heartbeat) messages */
 	@Override
 	public synchronized void receive(Message m) {
-		long recieveTime = System.currentTimeMillis();
-		long delay = (System.currentTimeMillis() - Long.parseLong(m.getPayload()));
-		int sourceProcess = m.getSource() - 1;
-		
-		processLastMessage[sourceProcess] = recieveTime;
-		
-		if (maximumDelays[sourceProcess] < delay){
-			maximumDelays[sourceProcess] = delay;
+		if (m.getType().equals("heartbeat")) {
+			long recieveTime = System.currentTimeMillis();
+			long delay = (System.currentTimeMillis() - Long.parseLong(m.getPayload()));
+			int sourceProcess = m.getSource() - 1;
+
+			processLastMessage[sourceProcess] = recieveTime;
+
+			if (maximumDelays[sourceProcess] < delay){
+				maximumDelays[sourceProcess] = delay;
+			}
+
+			if (suspects.contains(sourceProcess)){
+				suspects.remove(sourceProcess);
+			}
+			latestMessage = m;
+
+			Utils.out(process.pid, m.toString());
+			isSuspected(m.getSource());
 		}
-		
-		if (suspects.contains(sourceProcess)){
-			suspects.remove(sourceProcess);
-		}
-		latestMessage = m;
-		
-		Utils.out(process.pid, m.toString());
-		isSuspected(m.getSource());
 	}
 
 	/* Returns true if ‘process’ is suspected */
@@ -77,8 +79,8 @@ public class EventuallyPerfectFailureDetector implements IFailureDetector {
 	public boolean isSuspect(Integer process) {
 		return suspects.contains(process);
 	}
-	
-	
+
+
 	@Override
 	public int getLeader() {
 		return 0;
@@ -87,7 +89,7 @@ public class EventuallyPerfectFailureDetector implements IFailureDetector {
 	/* Notifies a blocking thread that ‘process’ has been suspected.
 	 * * Used only for tasks in §2.1.3 */
 	@Override
-	public void isSuspected(Integer process) {
+	public synchronized void isSuspected(Integer process) {
 		notifyAll();
 
 	}
--- src/EventuallyStrongFailureDetector.java
+++ src/EventuallyStrongFailureDetector.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventuallyStrongFailureDetector extends EventuallyPerfectFailureDetector {
-	
+
 	private Message messageCollected;
 	private boolean finished;
 	private int r = 0;
@@ -10,40 +10,50 @@ public class EventuallyStrongFailureDetector extends EventuallyPerfectFailureDet
 	private int i = process.pid;
 	private int n = process.n;
 	private int x = process.pid;
+	private int m = 0;
 	private int thresholdListSize;
 	private List<Message> messages;
-	
+
 	class ConsensusTask extends PeriodicTask {
-		
+
 		public void run() {
 			super.run();
-			
+
 			if (!finished){
 				r++;
 				c = (r % n) + 1;
 				messages.clear();
 				process.broadcast("VAL", Integer.toString(x) + "," + Integer.toString(r));
-				
+
 				if (i==c){
 					recieveMessages();
 					/* Majority and d need to be coded up */
 					int v = majority();
-					int d = -1;
-					process.broadcast("OUTCOME", Integer.toString(d) + ","+Integer.toString(v)
+					boolean d = isAllSameDecision();
+					process.broadcast("OUTCOME", Boolean.toString(d) + ","+Integer.toString(v)
 							+ "," + Integer.toString(r));
 				}
 				else if(collect("OUTCOME", c)){
 					String messagePayload = messageCollected.getPayload();
 					String [] tokens = messagePayload.split(",");
 					int value = Integer.parseInt(tokens[1]);
-					int currRound = Integer.parseInt(tokens[0]);
-					x = value;
+					boolean allSame = Boolean.parseBoolean(tokens[0]);
+					if (allSame){
+						x = value;
+						if (m == c){
+							finished = true;
+						}
+						else {
+							m = c;
+						}
+					}
+					
 				}		
 			}
 		}
 	}	
-	
-	private void recieveMessages(){
+
+	private synchronized void recieveMessages(){
 		while (messages.size() < thresholdListSize){
 			try {
 				wait();
@@ -60,33 +70,84 @@ public class EventuallyStrongFailureDetector extends EventuallyPerfectFailureDet
 		this.messages = new ArrayList<Message>();
 		this.thresholdListSize = n - (n-1)/3 - 1;
 	}
+
+	private boolean isAllSameDecision(){
+		int prevLeader = -1;
+		for (Message m: messages){
+			if (prevLeader == -1){
+				prevLeader = getLeaderFromMessage(m);
+			} else if (prevLeader != getLeaderFromMessage(m)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	
-	private int majority(){
-		return -1;
+	private int getRoundFromMessage(Message m){
+		String messagePayload = m.getPayload();
+		String [] tokens = messagePayload.split(",");
+		return Integer.parseInt(tokens[1]);
 	}
 	
+	private int getLeaderFromMessage(Message m){
+		String messagePayload = m.getPayload();
+		String [] tokens = messagePayload.split(",");
+		return Integer.parseInt(tokens[0]);
+	}
+
+	private int majority(){
+		int [] sumMajority = new int[n];
+		for (Message m : messages){
+			sumMajority[getLeaderFromMessage(m) - 1]++;
+		}
+
+		// Return the max
+		int majority = 0;
+		int max = sumMajority[majority];
+		for(int i = 1; i < sumMajority.length; i++){
+			if (max < sumMajority[i]){
+				majority = i;
+				max = sumMajority[i];
+			}
+		}
+
+		return majority;
+	}
+	
+	@Override
+	public synchronized void receive(Message m){
+		super.receive(m);
+		if (m.getType().equals("VAL")){
+			if (!suspects.contains(m.getSource()) && r == getRoundFromMessage(m)){
+				messages.add(m);
+				notifyAll();
+			}
+		}
+	}
+
 	private synchronized boolean collect(String value, int r){
 
-//		Utils.out("Reached collect");
+		//		Utils.out("Reached collect");
 		while (		!suspects.contains(r) && (
-						latestMessage == null ||
-						latestMessage.getSource() != r ||
-						!latestMessage.getType().equals(value)
-					)
+				latestMessage == null ||
+				latestMessage.getSource() != r ||
+				!latestMessage.getType().equals(value)
+				)
 				) {
 			try {
-//				Utils.out("I got blocked and whatever");
+				//				Utils.out("I got blocked and whatever");
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		if (latestMessage != null){
 			messageCollected = latestMessage;
 		}
-		
-//		Utils.out("I got unblocked");
+
+		//		Utils.out("I got unblocked");
 		return !isSuspect(r);
 	}
 
